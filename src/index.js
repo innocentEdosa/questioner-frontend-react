42c12c1cdba409d9ab8840a0856d897fb802e234
innocentEdosa <ilegbinijieinnocentcareer@gmail.com>
2019-05-09 18:32:12 +0100

    chore/165636282-setup-react-project

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+import './assets/bootstrap/bootstrap.min.css';
+import './assets/bootstrap/jquery.min';
+import './assets/sass/index.scss';
+
+
+render(<div> i am a working react app</div>, document.getElementById('root'));
