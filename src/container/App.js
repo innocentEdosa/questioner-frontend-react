fa2796f3b4e65e44a92e1f7907c38401baac4681
innocentEdosa <ilegbinijieinnocentcareer@gmail.com>
2019-05-09 20:54:40 +0100

    165917678(redux) setup redux

---
+++ src/container/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+const App = () => {
+  return (
+    <div>This is the react front-end for questioner</div>
+  );
+};
+
+export default connect()(App);
