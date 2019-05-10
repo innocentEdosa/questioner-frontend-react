abbbf5b076a157503be6bfc294228f74bdd15a6d
innocentEdosa <ilegbinijieinnocentcareer@gmail.com>
2019-05-10 17:56:29 +0100

    165893770(nav) create app navigation

--- src/container/App.js
+++ src/container/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Route, Switch } from 'react-router-dom';
+import Navigation from '../components/Navigation';
 import Home from '../components/Home';
-import Login from '../components/Login';
+import Login from './Login';
+import Signup from './Signup';
 
 
 const App = () => (
-  <Switch>
-    <Route path="/" exact component={Home} />
-    <Route path="/login" component={Login} />
-  </Switch>
+  <div>
+    <Navigation />
+    <Switch>
+      <Route path="/" exact component={Home} />
+      <Route path="/login" component={Login} />
+      <Route path="/signup" component={Signup} />
+    </Switch>
+  </div>
 );
 
 export default connect()(App);
---
+++ src/container/Login.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Login = () => <div> this is a new login page </div>;
+
+export default Login;
